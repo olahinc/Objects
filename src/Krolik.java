bdbe06f8b0771be389a1dd765956cab9b58d42e2
Aleksandra <aleksandra.hinc00@gmail.com>
2022-10-26 19:29:56 +0200

    c

---
+++ src/Krolik.java
@@ -0,0 +1,11 @@
+public class Krolik {
+    int length;
+    String name;
+    boolean softness;
+
+    Krolik(int lenght, String name, boolean softness) {
+        this.length=lenght;
+        this.name=name;
+        this.softness=softness;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Krolik krolik1= new Krolik(50, "Ziomek", true);
+    }
+
+}
