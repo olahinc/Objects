3eae056cf6590bbd2b118e73f7635b90acc00fa3
Aleksandra <aleksandra.hinc00@gmail.com>
2022-10-26 20:51:11 +0200

    przeliczanie na cale

--- src/Krolik.java
+++ src/Krolik.java
@@ -4,14 +4,34 @@ public class Krolik {
     private boolean softness;
 
     Krolik(int lenght, String name, boolean softness) {
-        this.length=lenght;
-        this.name=name;
-        this.softness=softness;
+        this.length = lenght;
+        this.name = name;
+        this.softness = softness;
     }
+
     public void changeName(String name) {
-        this.name=name;
+        this.name = name;
     }
-    public String viewName () {
+
+    public String viewName() {
         return name;
     }
+    public void changeSoftness(boolean softness) {
+        this.softness = softness;
+    }
+    public boolean viewSoftness () {
+        return softness;
+    }
+
+    public void changeLength(int length) {
+        this.length=length;
+    }
+    public int viewLength(){
+        return length;
+    }
+    public double convert(int lenght) {
+        double cale=length*2.54;
+        return cale;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
         Krolik krolik1= new Krolik(50, "Ziomek", true);
         krolik1.changeName("Lolek");
         System.out.println(krolik1.viewName());
+        krolik1.changeSoftness(false);
+        System.out.println(krolik1.viewSoftness());
+        krolik1.changeLength(30);
+        System.out.println(krolik1.viewLength());
+        System.out.println(krolik1.convert(krolik1.viewLength()));
     }
 
 }
